0d6191136aee1d4b6211874ded65e72e21674e6c
Mrityunjoy panday <118350526+Mrjoypanday@users.noreply.github.com>
2023-08-14 17:34:12 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -2,7 +2,12 @@ import "./App.css";
 import { BrowserRouter as Routers, Routes, Route } from "react-router-dom";
 import Home from "./page/Home";
 import Blog from "./page/Blog";
+import { useLocation } from "react-router-dom";
 function App() {
+   const { pathname } = useLocation();
+  useEffect(() => {
+    window.scrollTo({ top: 0, behavior: "smooth" });
+  }, [pathname]);
   return (
     <Routers>
       <Routes>
